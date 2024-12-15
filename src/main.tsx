7253461115777c0d362f1ab6d66340af7682c8e9
Vidit Naithani <102232954+ViditNaithani22@users.noreply.github.com>
2024-12-14 23:51:30 -0500

    signin signout

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
+import '@aws-amplify/ui-react/styles.css';
+import { Authenticator } from '@aws-amplify/ui-react';
 
 Amplify.configure(outputs);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <Authenticator>
     <App />
+    </Authenticator>
   </React.StrictMode>
 );
--- src/partials/NavBar.tsx
+++ src/partials/NavBar.tsx
@@ -2,17 +2,27 @@
 import {Link} from 'react-router-dom';
 import {ShoppingCart} from 'phosphor-react';
 import './NavBar.css';
-
+import { useAuthenticator } from '@aws-amplify/ui-react';
 
 export default function NavBar(){
+    
+    const {signOut} = useAuthenticator();
+
     return(
         <div className="navbar">
             <div>
                  <Link to="/" style={{color: "white", fontSize: "22px", textDecoration: "none", marginLeft:"30px"}}><b>CutePots</b></Link>
                  
             </div>
+
+            
+                  
+            
             
             <div className='nav-items'>
+
+                <b  onClick={signOut} style={{color: "white", fontSize: "22px", textDecoration: "none", marginRight:"30px", cursor: "pointer"}}>Sign Out</b>
+
                 <Link to="/cart" style={{color: "white"}}>
                     <ShoppingCart size={32}/>
                  </Link>
